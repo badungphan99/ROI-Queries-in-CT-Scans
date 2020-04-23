ea5624ec0e4990d402c63485963107e4236b0382
dungpb-fti <badungphan99@gmail.com>
2020-04-23 11:03:44 +0700

    update features extract to csv

---
+++ getjsoninfo.py
@@ -0,0 +1,107 @@
+import json
+import csv
+import os
+
+
+def extract_csv(parent_dir_path: str, json_path: str, features_out_path: str):
+    regions = ["head", "chest", "abdominal"]
+
+    with open(json_path) as f:
+        data = json.load(f)
+
+    print(len([f for f in os.listdir(parent_dir_path) if os.path.isfile(os.path.join(parent_dir_path, f))]))
+
+    for image in data["images"]:
+        head_begin = -1
+        head_end = -1
+        chest_begin = -1
+        chest_end = -1
+        abdominal_begin = -1
+        abdominal_end = -1
+        num_img = -1
+        patient_dir = ""
+        if "patient_dir" in image:
+            patient_dir = os.path.join(parent_dir_path, image["patient_dir"])
+            num_img = len([f for f in os.listdir(patient_dir) if os.path.isfile(os.path.join(patient_dir, f))])
+        for r in regions:
+            if r in image:
+                if "begin" in image[r]:
+                    if r == "head":
+                        head_begin = image[r]["begin"]
+                    if r == "chest":
+                        chest_begin = image[r]["begin"]
+                    if r == "abdominal":
+                        abdominal_begin = image[r]["begin"]
+                else:
+                    print("ERROR: no", r, "begin in", image["patient_dir"])
+                    return False
+                if "end" in image["head"]:
+                    if r == "head":
+                        head_end = image[r]["end"]
+                    if r == "chest":
+                        chest_end = image[r]["end"]
+                    if r == "abdominal":
+                        abdominal_end = image[r]["end"]
+                else:
+                    print("ERROR: no", r, "end in", image["patient_dir"])
+                    return False
+
+        patient = Patient(patient_dir, num_img, head_begin, head_end, chest_begin, chest_end, abdominal_begin,abdominal_end)
+        patient.init_features()
+        patient.write_csv(features_out_path)
+
+
+class Patient:
+    def __init__(self, patient_dir: str, num_img: int, head_begin: int, head_end: int, chest_begin: int, chest_end: int,
+                 abdominal_begin: int, abdominal_end: int):
+        self.__patient_dir = patient_dir
+        self.__num_img = num_img
+        self.__head_begin = head_begin
+        self.__head_end = head_end
+        self.__chest_begin = chest_begin
+        self.__chets_end = chest_end
+        self.__abdominal_begin = abdominal_begin
+        self.__abdominal_end = abdominal_end
+        self.__features = []
+
+    def init_features(self):
+        pass
+
+    def write_csv(self, csv_path: str):
+        with open(csv_path, 'a') as csv_file:
+            wr = csv.writer(csv_file, delimiter=',')
+            for i in range(0, self.__num_img):
+                region = 0
+                region_begin = 0.1
+                region_end = 0.2
+                if self.__head_begin <= i <= self.__head_end:
+                    region = 1
+                if self.__head_begin <= i <= self.__head_begin + 5:
+                    wr.writerow([str(1.1)] + list(self.__features[i]))
+                if self.__head_end - 5 <= i <= self.__head_end:
+                    wr.writerow([str(1.2)] + list(self.__features[i]))
+
+                if self.__chest_begin <= i <= self.__chets_end:
+                    region = 2
+                if self.__chest_begin <= i <= self.__chest_begin + 5:
+                    wr.writerow([str(2.1)] + list(self.__features[i]))
+                if self.__chets_end - 5 <= i <= self.__chets_end:
+                    wr.writerow([str(2.2)] + list(self.__features[i]))
+
+                if self.__abdominal_begin <= i <= self.__abdominal_end:
+                    region = 3
+                if self.__abdominal_begin <= i <= self.__head_begin + 5:
+                    wr.writerow([str(3.1)] + list(self.__features[i]))
+                if self.__abdominal_end - 5 <= i <= self.__abdominal_end:
+                    wr.writerow([str(3.2)] + list(self.__features[i]))
+
+                wr.writerow([str(region)] + list(self.__features[i]))
+
+
+
+if __name__ == "__main__":
+    parent_dir = "/home/dungpb/HEAD_007/crop"
+    json_path = "/home/dungpb/Work/ROI-Queries-in-CT-Scans/annotations/template.json"
+    csv_path = "abc"
+
+    extract_csv(parent_dir, json_path, csv_path)
